af12aa99f8015eff1d670ec168eac9ca0a52189e
alanBonnet <84151943+alanBonnet@users.noreply.github.com>
2022-11-21 20:46:32 -0300

    protección de rutas y AuthContext agregado, limpieza de css vite

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -2,13 +2,23 @@ import { useState } from "react";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { Routes, Route } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
+import  {PublicRoutes}  from "./routes/PublicRoutes";
+import LoginRoutes from "./routes/LoginRoutes";
 
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/Register" element={<Register />} />
-    </Routes>
-  );
+    const [user,setUser] = useState({
+        isLogged:false
+    })
+    return (
+        <AuthContext.Provider value={{
+            user,
+            setUser
+        }}>
+            <PublicRoutes>
+                <LoginRoutes/>
+            </PublicRoutes>
+        </AuthContext.Provider>
+    );
 }

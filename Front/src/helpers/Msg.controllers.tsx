dbfaa12fe3c5bbd5f6fac8d50742dbfe24a5ca93
alanBonnet <84151943+alanBonnet@users.noreply.github.com>
2022-11-22 21:27:39 -0300

    mejora en tipado de dato en msg controller jsx a tsx

---
+++ Front/src/helpers/Msg.controllers.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+const existError = (error:any, comparer:string):JSX.Element => {
+    if(error){
+        return (<div className="row">
+        {error?.map((errors:any, index:number) => {
+            if (errors.param == comparer) {
+                return <p className="alert alert-danger my-1 mx-1 text-center p-1 col" key={errors.value + index}>
+                    {`${errors.msg}`}
+                </p>
+            }
+        })}
+    </div>)
+    }
+    return (<></>)
+    
+    
+}
+
+export default existError 

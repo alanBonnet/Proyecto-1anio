e097fd58a8b1d8c9192870da3d9677e39f8957ee
alanBonnet <84151943+alanBonnet@users.noreply.github.com>
2022-12-07 02:43:11 -0300

    funcionalidad xd en mesa de trabajo

--- Front/src/components/CheckBox.tsx
+++ Front/src/components/CheckBox.tsx
@@ -1,11 +1,11 @@
-import React, { Dispatch, FC, MutableRefObject, SetStateAction } from "react";
-interface Props {
-  name:string,
-  checked:boolean,
-  setChecked:Dispatch<SetStateAction<Boolean>>,
-  refCheckbox:MutableRefObject<any>
+import React, { Dispatch, FC, MutableRefObject, SetStateAction, useState } from "react";
+interface PropsCheckbox {
+  name: string,
+  id:number,
+  checked: boolean,
+  setChecked: Dispatch<SetStateAction<boolean>>
 }
- const CheckBox:FC<Props> = ({ name, checked, setChecked, refCheckbox}) => {
+const CheckBox: FC<PropsCheckbox> = ({ name,id, checked, setChecked }) => {
   return (
     <>
       <input
@@ -13,11 +13,41 @@ interface Props {
         className="form-check-input"
         type="checkbox"
         checked={checked}
-        id={name}
-        onChange={()=>{setChecked(!checked)}}
+        id={`${name}_${id}`}
+        onChange={() => { setChecked(!checked) }}
       />
-      <label className="px-4 form-check-label" htmlFor={name}>{name}</label>
+      <label className="px-4 form-check-label" htmlFor={`${name}_${id}`}>{name}</label>
     </>
   );
 }
-export default CheckBox;
+interface GroupCheckbox {
+  name1: string,
+  name2: string,
+  name3: string,
+  id1:number,
+  id2:number,
+  id3:number,
+
+}
+const Group3CheckBoxes: FC<GroupCheckbox> = ({ name1, name2, name3,id1,id2,id3 }) => {
+  const [checked_1, setCheckbox_1] = useState(false);
+  const [checked_2, setCheckbox_2] = useState(false);
+  const [checked_3, setCheckbox_3] = useState(false);
+  return (
+    <>
+      <div className="form-check form-switch d-flex align-content-center">
+        <CheckBox name={name1} id={1+id1} checked={checked_1} setChecked={setCheckbox_1} />
+      </div>
+      <div className="form-check form-switch d-flex align-content-center">
+        <CheckBox name={name2} id={2+id2} checked={checked_2} setChecked={setCheckbox_2} />
+      </div>
+      <div className="form-check form-switch d-flex align-content-center">
+        <CheckBox name={name3} id={3+id3} checked={checked_3} setChecked={setCheckbox_3} />
+      </div>
+
+    </>
+
+  )
+}
+
+export default Group3CheckBoxes;
--- Front/src/pages/Home.jsx
+++ Front/src/pages/Home.jsx
@@ -5,223 +5,233 @@ import axios from "axios";
 import CheckBox from "../components/CheckBox";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faUserCheck,
-  faUserXmark,
-  faUserMinus,
+    faUserCheck,
+    faUserXmark,
+    faUserMinus,
 } from "@fortawesome/free-solid-svg-icons";
+import Group3CheckBoxes from "../components/CheckBox";
 
 export default function Home() {
-  //useREFS
-  const changeFileUpload = useRef();
-  const resultText = useRef();
-  // useStates
-  const [cont, setCont] = useState(0);
-  const [linkSubida, setLinkSubida] = useState(null);
-  const [textoPDF, setTextoPDF] = useState("");
-  const [checkboxJavascript, setCheckboxJavascript] = useState(false);
-  const [checkboxPython, setCheckboxPython] = useState(false);
-  const [checkboxStSkills, setCheckboxStSkills] = useState(false);
-  //functions
+    //useREFS
+    const changeFileUpload = useRef();
+    const resultText = useRef();
+    // useStates
+    const [cont, setCont] = useState(0);
+    const [linkSubida, setLinkSubida] = useState(null);
+    const [textoPDF, setTextoPDF] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
+    const [isSearching, setIsSearching] = useState(false);
+    const [isLoading2, setIsLoading2] = useState(true);
+    const [isSearching2, setIsSearching2] = useState(false);
+    const [isLoading3, setIsLoading3] = useState(true);
+    const [isSearching3, setIsSearching3] = useState(false);
+    //functions
 
-  async function onChangeFile() {
-    if (changeFileUpload.current?.files[0]) {
-      await diccionarioCV["convertir PDF a Texto"](
-        changeFileUpload.current.files[0],
-        setTextoPDF
-      );
-      diccionarioCV["guardar archivo"](
-        changeFileUpload.current.files[0],
-        cont,
-        setCont,
-        setLinkSubida
-      );
+    async function onChangeFile() {
+        if (changeFileUpload.current?.files[0]) {
+            const result = await diccionarioCV["convertir PDF a Texto"](
+                changeFileUpload.current.files[0],
+                setTextoPDF
+            );
+            diccionarioCV["guardar archivo"](
+                changeFileUpload.current.files[0],
+                cont,
+                setCont,
+                setLinkSubida
+            );
+            if (result) {
+                return true
+            }
+        }
     }
-  }
 
-  function Transformar() {
-    axios.get("http://localhost:3000/subirArchivo").then((response) => {
-      console.log(response.data.msg);
-    });
-  }
-  const guardarArchivoDeTexto = (contenido, nombre) => {
-    const a = React.createElement("a");
-    const archivo = new Blob([contenido], { type: "" });
-    const url = URL.createObjectURL(archivo);
-  };
-  /* 
-      const guardarArchivoDeTexto = (contenido, nombre) => {
-          const a = document.createElement("a");
-          const archivo = new Blob([contenido], { type: '' });
-          const url = URL.createObjectURL(archivo);
-          a.href = url;
-          a.download = nombre;
-          a.click();
-          URL.revokeObjectURL(url);
-      }
-      const $botonDescargar = document.querySelector("#descargar");
-      $botonDescargar.onclick = () => {
-          guardarArchivoDeTexto(pruebaJSON, "html.json");
-  
-      }
-    */
+    function Transformar() {
+        axios.get("http://localhost:3000/subirArchivo").then((response) => {
+            console.log(response.data.msg);
+        });
+    }
+    const guardarArchivoDeTexto = (contenido, nombre) => {
+        const a = React.createElement("a");
+        const archivo = new Blob([contenido], { type: "" });
+        const url = URL.createObjectURL(archivo);
+    };
+    /* 
+        const guardarArchivoDeTexto = (contenido, nombre) => {
+            const a = document.createElement("a");
+            const archivo = new Blob([contenido], { type: '' });
+            const url = URL.createObjectURL(archivo);
+            a.href = url;
+            a.download = nombre;
+            a.click();
+            URL.revokeObjectURL(url);
+        }
+        const $botonDescargar = document.querySelector("#descargar");
+        $botonDescargar.onclick = () => {
+            guardarArchivoDeTexto(pruebaJSON, "html.json");
+    
+        }
+      */
 
-  //useEffect
-  useEffect(() => {
-    // resultText.current.value = textoPDF;
-    console.log(textoPDF);
-  }, [textoPDF]);
-  return (
-    <div className="row">
-      <div className="col-3">
-        <Dashboard />
-      </div>
-
-      <main className=" col-9 main-content position-relative max-height-vh-100 h-100  border-radius-lg ">
-        <nav
-          className="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl"
-          id="navbarBlur"
-          navbar-scroll="true"
-        ></nav>
-        <div className="container-fluid py-4">
-          <input type="file" id="fileUpload" ref={changeFileUpload} />
-
-          <button type="button" id="btnUpload" onClick={onChangeFile}>
-            Subir a Base de datos
-          </button>
-          <button
-            id="descargar"
-            onClick={() => {
-              Transformar();
-            }}
-          >
-            Transformar
-          </button>
-          <br />
-          <br />
-          <div className="col-12 col-md-12">
-            <h4>Que estas buscando en el CV?</h4>
-            <div className="row">
-              <div className="col-3">
-                <p>Habilidades</p>
-                <div className="form-check form-switch d-flex align-content-center">
-                  <CheckBox
-                    name="JavaScript"
-                    checked={checkboxJavascript}
-                    setChecked={setCheckboxJavascript}
-                  />
-                </div>
-                <div className="form-check form-switch d-flex align-content-center">
-                  <CheckBox
-                    name="Python"
-                    checked={checkboxPython}
-                    setChecked={setCheckboxPython}
-                  />
-                </div>
-                <div className="form-check form-switch d-flex align-content-center">
-                  <CheckBox
-                    name="Habilidades Blandas"
-                    checked={checkboxStSkills}
-                    setChecked={setCheckboxStSkills}
-                  />
-                </div>
-              </div>
-              <div className="col-3">
-                <p>Experiencia</p>
-                <div className="form-check form-switch">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    id="flexSwitchCheckDefault"
-                  />
-                  <label
-                    className="form-check-label"
-                    htmlFor="flexSwitchCheckDefault"
-                  >
-                    Default switch checkbox input
-                  </label>
-                </div>
-              </div>
-              <div className="col-3">
-                {" "}
-                <p>Educacion</p>
-                <div className="form-check form-switch">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    id="flexSwitchCheckDefault"
-                  />
-                  <label
-                    className="form-check-label"
-                    htmlFor="flexSwitchCheckDefault"
-                  >
-                    Default switch checkbox input
-                  </label>
-                </div>
-              </div>
-              <div className="col-3">
-                <p>Lenguajes</p>
-                <div className="form-check form-switch">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    id="flexSwitchCheckDefault"
-                  />
-                  <label
-                    className="form-check-label"
-                    htmlFor="flexSwitchCheckDefault"
-                  >
-                    Default switch checkbox input
-                  </label>
-                </div>
-              </div>
+    //useEffect
+    useEffect(() => {
+        // resultText.current.value = textoPDF;
+        console.log(textoPDF);
+    }, [textoPDF]);
+    return (
+        <div className="row">
+            <div className="col-3">
+                <Dashboard />
             </div>
-            <div className="row mt-5">
-              <div class="col-4 container-01">
-                <div class="neumorphic-card">
-                  <div class="contentBox">
-                    <div className="social-media-icon">
-                      <FontAwesomeIcon icon={faUserCheck} />
+
+            <main className=" col-9 main-content position-relative max-height-vh-100 h-100  border-radius-lg ">
+                <nav
+                    className="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl"
+                    id="navbarBlur"
+                    navbar-scroll="true"
+                ></nav>
+                <div className="container-fluid py-4">
+                    <input type="file" id="fileUpload" ref={changeFileUpload} />
+
+                    <button type="button" id="btnUpload"
+                        onClick={() => {
+                            if (changeFileUpload.current?.files[0]) {
+                                // se lanza si hay un pdf en files
+                                setIsSearching(false)
+                                setIsSearching3(false)
+                                setIsSearching2(true)
+                                setIsLoading2(true)
+                                setTimeout(() => {
+                                    setIsLoading2(false)
+                                    setTimeout(async() => {
+                                        await onChangeFile()
+                                        setIsSearching2(false)
+                                    }, 2100)
+                                }, 2700)
+                            }else{
+                                // en caso de no cargar un pdf
+                                setIsSearching(false)
+                                setIsSearching2(false)
+                                setIsSearching3(true)
+                                setIsLoading3(true)
+                                setTimeout(() => {
+                                    setIsLoading3(false)
+                                    setTimeout(() => {
+                                        setIsSearching3(false)
+                                    }, 2100)
+                                }, 2700)
+                            }
+                        }}
+                    >
+                        Subir a Base de datos
+                    </button>
+                    <button
+                        id="descargar"
+                        onClick={async () => {
+                            setIsSearching(true)
+                            setIsSearching2(false)
+                            setIsSearching3(false)
+                            setIsLoading(true)
+
+                            setTimeout(() => {
+                                setIsLoading(false)
+                                Transformar();
+                            }, 3000)
+                        }}
+                    >
+                        Transformar
+                    </button>
+                    <br />
+                    <br />
+                    <div className="col-12 col-md-12">
+                        <h4>Que estas buscando en el CV?</h4>
+                        <div className="row">
+                            <div className="col-3">
+                                <p>Habilidades</p>
+                                <Group3CheckBoxes name1="Javascript" id1={1} id2={2} id3={3} name2="Python" name3="Habilidades blandas" />
+                            </div>
+                            <div className="col-3">
+                                <p>Experiencia</p>
+                                <Group3CheckBoxes name1="Javascript" id1={4} id2={5} id3={6} name2="Python" name3="Liderazgo" />
+                            </div>
+                            <div className="col-3">
+                                {" "}
+                                <p>Educacion</p>
+                                <Group3CheckBoxes name1="Javascript" id1={7} id2={8} id3={9} name2="Python" name3="C++" />
+                            </div>
+                            <div className="col-3">
+                                <p>Lenguajes</p>
+                                <Group3CheckBoxes name1="Inglés" id1={1} id2={2} id3={3} name2="Chino Mandarín" name3="Alemán" />
+
+                            </div>
+                        </div>
+                        {!isSearching3 ? "" : isLoading3 ? <div className="d-flex justify-content-center mt-5">
+                            <div className="spinner-border" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </div>
+                        </div> : <div className="alert alert-danger text-center text-white fs-5 mt-5" role="alert">
+                            Ingrese un CV con formato PDF
+                        </div>
+                        }
+                        {!isSearching2 ? "" : isLoading2 ? <div className="d-flex justify-content-center mt-5">
+                            <div className="spinner-border" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </div>
+                        </div> : <div className="alert alert-success text-center text-dark fs-5 mt-5" role="alert">
+                            Subido correctamente a la base de datos
+                        </div>
+                        }
+                        {!isSearching ? "" : isLoading ? <div className="d-flex justify-content-center mt-5">
+                            <div className="spinner-border" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </div>
+                        </div>
+                            : <div className="row mt-5">
+                                <div className="col-4 container-01">
+                                    <div className="neumorphic-card">
+                                        <div className="contentBox">
+                                            <div className="social-media-icon">
+                                                <FontAwesomeIcon icon={faUserCheck} />
+                                            </div>
+                                            <h3>100%</h3>
+                                            <p>
+                                                Sebastian Prieto tiene un excelente coincidencia con la
+                                                busqueda realizada
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-4 container-01">
+                                    <div class="neumorphic-card">
+                                        <div class="contentBox">
+                                            <div className="social-media-icon">
+                                                <FontAwesomeIcon icon={faUserXmark} />
+                                            </div>
+                                            <h3>30%</h3>
+                                            <p>
+                                                Lautaro Durante tiene una coincidencia mala con la
+                                                busqueda realizada
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-4 container-01">
+                                    <div class="neumorphic-card">
+                                        <div class="contentBox">
+                                            <div className="social-media-icon">
+                                                <FontAwesomeIcon icon={faUserMinus} />
+                                            </div>
+                                            <h3>Design</h3>
+                                            <p>
+                                                Alan Bonnet tiene una coincidencia regular con la busqueda
+                                                realizada
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>}
                     </div>
-                    <h3>100%</h3>
-                    <p>
-                      Sebastian Prieto tiene un excelente coincidencia con la
-                      busqueda realizada
-                    </p>
-                  </div>
                 </div>
-              </div>
-              <div class="col-4 container-01">
-                <div class="neumorphic-card">
-                  <div class="contentBox">
-                    <div className="social-media-icon">
-                      <FontAwesomeIcon icon={faUserXmark} />
-                    </div>
-                    <h3>30%</h3>
-                    <p>
-                      Lautaro Durante tiene una coincidencia mala con la
-                      busqueda realizada
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-4 container-01">
-                <div class="neumorphic-card">
-                  <div class="contentBox">
-                    <div className="social-media-icon">
-                      <FontAwesomeIcon icon={faUserMinus} />
-                    </div>
-                    <h3>Design</h3>
-                    <p>
-                      Alan Bonnet tiene una coincidencia regular con la busqueda
-                      realizada
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+            </main>
         </div>
-      </main>
-    </div>
-  );
+    );
 }
